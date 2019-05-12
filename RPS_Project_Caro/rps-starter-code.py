acc28a9dbc4eda8b9aa70b1056b7f6144101fea1
CSJWL <carolien_luger@hotmail.com>
2019-05-12 05:03:19 -0700

    keeps track of scores and prints the scores

--- RPS_Project_Caro/rps-starter-code.py
+++ RPS_Project_Caro/rps-starter-code.py
@@ -35,16 +35,29 @@ class Game:
         self.p1 = p1
         self.p2 = p2
 
+        #create variables that will keep track of the scores
+        self.p1_wins = 0
+        self.p2_wins = 0
+        self.ties = 0
+
     def play_round(self):
         move1 = self.p1.move()
         move2 = self.p2.move()
         print(f"Player 1: {move1}  Player 2: {move2}")
         if beats(move1, move2):
             print('player1 wins')
+
+            #increase the value of the variable that keep track of the wins of player 1
+            self.p1_wins += 1
         elif move1 == move2:
-            print('tides')
+            print('ties')
+            #increase the value of the variable that keep track of the ties
+            self.ties += 1
         else:
             print('player2 wins')
+
+            #increase the value of the variable that keep track of the wins of player 2
+            self.p2_wins += 1
 
         self.p1.learn(move1, move2)
         self.p2.learn(move2, move1)
@@ -55,6 +68,9 @@ class Game:
             print(f"Round {round}:")
             self.play_round()
         print("Game over!")
+
+        #display the outcome of each round
+        print(f" Player1 has won {self.p1_wins} times, Player 2 has won {self.p2_wins} times and there was/were {self.ties} ties")
 
 if __name__ == '__main__':
     print("hoi")
