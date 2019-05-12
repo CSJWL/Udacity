0cfaec81054e804813700fc837a40c074a34a97c
CSJWL <carolien_luger@hotmail.com>
2019-05-12 04:37:26 -0700

    added random module, created randomplayer subclass, add printstatements who won the game

---
+++ RPS_Project_Caro/rps-starter-code.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+
+"""This program plays a game of Rock, Paper, Scissors between two Players,
+and reports both Player's scores each round."""
+
+#import the random module to create a move that will be able to return the moves at random
+import random
+
+moves = ['rock', 'paper', 'scissors']
+
+"""The Player class is the parent class for all of the Players
+in this game"""
+
+class Player:
+    def move(self):
+        return 'rock'
+
+    def learn(self, my_move, their_move):
+        pass
+
+#create a randomplayer as a subclass of player. The randomplayer returns one of 3 possible moves at random.
+class RandomPlayer(Player):
+    def move(self):
+        print("in randomplayer")
+        return random.choice(moves)
+
+def beats(one, two):
+    return ((one == 'rock' and two == 'scissors') or
+            (one == 'scissors' and two == 'paper') or
+            (one == 'paper' and two == 'rock'))
+
+
+class Game:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+
+    def play_round(self):
+        move1 = self.p1.move()
+        move2 = self.p2.move()
+        print(f"Player 1: {move1}  Player 2: {move2}")
+        if beats(move1, move2):
+            print('player1 wins')
+        elif move1 == move2:
+            print('tides')
+        else:
+            print('player2 wins')
+
+        self.p1.learn(move1, move2)
+        self.p2.learn(move2, move1)
+
+    def play_game(self):
+        print("Game start!")
+        for round in range(3):
+            print(f"Round {round}:")
+            self.play_round()
+        print("Game over!")
+
+if __name__ == '__main__':
+    print("hoi")
+    game = Game(RandomPlayer(), Player())
+    game.play_game()
